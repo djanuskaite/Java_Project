b4e7632b67b9db8f085d02b56f40746e775f17c5
DOMPOC <71129324+DOMPOC@users.noreply.github.com>
2020-10-16 09:49:03 +0300

    Initial commit.

---
+++ app/src/main/java/com/corona/coronazp20t/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.corona.coronazp20t;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//------jeigu raudonuoja, nera bibliotekos, ALT+Enter-----//
+
+//------VOID - negrazinanti funkcija----------------------//
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
